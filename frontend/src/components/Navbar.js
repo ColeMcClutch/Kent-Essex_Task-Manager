a48048cc6dc675c12f690e55e270d3d8e2b054e9
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2025-03-01 14:49:16 -0500

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Task Manager</h1>
+      <ul>
+        <li><a href="/">Home</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
