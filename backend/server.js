2cdc7c37f8824af6b97e4bd79debe71a60a11a97
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2025-03-01 12:00:28 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const app = require("./src/app");
+const connectDB = require("./src/config/db"); // Import DB connection
 
 const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB
+connectDB();
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
