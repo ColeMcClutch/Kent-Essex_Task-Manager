15762f0628300583a2c36c1f16d9441e27ed7132
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2025-03-02 12:30:18 -0500

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,14 @@
+//Connect to mongoDB
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+//Async connection
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected Successfully");
+    console.log("✅ MongoDB Connected Successfully"); //Message for successful mongo connection
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error);
+    console.error("❌ MongoDB Connection Failed:", error); //Message for failed mongo connection
     process.exit(1); // Exit process with failure
   }
 };
