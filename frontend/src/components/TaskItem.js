669d06e32559aa8669cd168534c9ae7a0eb96c06
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2025-03-01 20:22:32 -0500

    Update TaskItem.js

--- frontend/src/components/TaskItem.js
+++ frontend/src/components/TaskItem.js
@@ -1,12 +1,14 @@
+import "./TaskItem.css"; // Import CSS for styling
+
 const TaskItem = ({ task, onUpdate, onDelete }) => {
   return (
-    <div className="task-card">
+    <li className="task-card">
       <h3>{task.title}</h3>
       <p><strong>Status:</strong> {task.status}</p>
-      <button onClick={() => onUpdate(task._id, "in-progress")}>In Progress</button>
-      <button onClick={() => onUpdate(task._id, "completed")}>Complete</button>
+      <button className="progress" onClick={() => onUpdate(task._id, "in-progress")}>In Progress</button>
+      <button className="complete" onClick={() => onUpdate(task._id, "completed")}>Complete</button>
       <button className="delete" onClick={() => onDelete(task._id)}>Delete</button>
-    </div>
+    </li>
   );
 };
 
