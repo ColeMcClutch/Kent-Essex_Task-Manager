f58e065dacb7f8664b5cbacece1a4c5de98c01f8
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2025-03-02 12:31:14 -0500

    Update tasks.js

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -1,22 +1,26 @@
-import axios from "axios";
+import axios from "axios"; //import axios
 
 const API_URL = "http://localhost:5000/api/tasks"; // Adjust if needed
 
+//getTasks funciton
 export const getTasks = async () => {
   const response = await axios.get(API_URL);
   return response.data;
 };
 
+//create tasks function
 export const createTask = async (title) => {
   const response = await axios.post(API_URL, { title });
   return response.data;
 };
 
+//updateTasks function
 export const updateTask = async (id, status) => {
   const response = await axios.put(`${API_URL}/${id}`, { status });
   return response.data;
 };
 
+//deleteTasks function
 export const deleteTask = async (id) => {
   await axios.delete(`${API_URL}/${id}`);
 };
