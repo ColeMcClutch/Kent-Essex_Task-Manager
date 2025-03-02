986d1c08fc61ffcda6cce25fdce1be6b2f90948a
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2025-03-02 12:30:24 -0500

    Update taskModel.js

--- backend/src/models/taskModel.js
+++ backend/src/models/taskModel.js
@@ -1,11 +1,14 @@
+//Creating the model for a task object
+//Connect to mongoode
 const mongoose = require("mongoose");
 
+//Set the schema
 const taskSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    status: { type: String, enum: ["pending", "in-progress", "completed"], default: "pending" },
+    title: { type: String, required: true }, //Task name or objective
+    status: { type: String, enum: ["pending", "in-progress", "completed"], default: "pending" }, //Set status as pending as default and give options to change
   },
-  { timestamps: true }
+  { timestamps: true } //timestamp means it exists
 );
 
 module.exports = mongoose.model("Task", taskSchema);
