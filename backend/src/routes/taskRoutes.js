6b83305c6ca7b1543cbfcd87d04e408abb90e2af
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2025-03-02 12:30:28 -0500

    Update taskRoutes.js

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,8 +1,11 @@
+//Establish routes through Task creation
 const express = require("express");
 const { createTask, getTasks, updateTask, deleteTask } = require("../controllers/taskController");
 
+//Create router
 const router = express.Router();
 
+//Establish routes for operations
 router.post("/", createTask);
 router.get("/", getTasks);
 router.put("/:id", updateTask);
