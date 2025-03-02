c218a7643589b2417f6cf60fed7855671b8ced41
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2025-03-02 12:31:23 -0500

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,12 @@
+//Connect to Navbar.css
 import "./Navbar.css";
 
+//Creates model for title bar at the top
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Task Manager</h1>
       <ul>
-        <li><a href="/">Home</a></li>
       </ul>
     </nav>
   );
