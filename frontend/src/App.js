70e5c0e8e6f605de07cf310d53830c7c22ecc302
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2025-03-02 12:31:17 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+//imports to other codes
 import Navbar from "./components/Navbar";
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 import "./App.css";
 
+//App functions
 function App() {
   return (
     <div className="App">
